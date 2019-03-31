40779762087f5022deea4197d6f64830df906997
bcjohn <bcjohnblue@gmail.com>
2019-03-31 21:08:05 +0800

    deleteCart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -7,7 +7,10 @@
         <p class="description">{{ row.description }}</p>
         <p class="price">{{ "$ " + row.price }}</p>
       </template>
-      <template #controls="{ row }">數量: {{ row.quantity }}</template>
+      <template #controls="{ row }">
+        <span class="quantity">數量: {{ row.quantity }}</span>
+        <el-button type="danger" @click="deleteClick(row.productId)">刪除</el-button>
+      </template>
     </group-row-item>
   </div>
 </template>
@@ -83,8 +86,41 @@ export default class Product extends Vue {
       this.$message.error(err.message);
     }
   }
+
+  /**
+   * deleteClick
+   */
+  public async deleteClick(productId: string) {
+    try {
+      const userId = this.$store.getters.userId;
+      console.log(userId, productId);
+
+      const response = await this.$apollo.mutate({
+        mutation: gql`mutation ($userId: ID!, $productId: ID!) {
+            deleteCart(userId: $userId, productId: $productId) {
+              userId
+            }
+          }`,
+        variables: {
+          userId,
+          productId
+        }
+      });
+      console.log(response.data);
+      this.$message.success('刪除成功!')
+      this.getCart()
+      // this.rowItem.data = response.data.getProducts;
+
+    } catch (err) {
+      this.$message.error(err.message);
+    }
+
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.quantity {
+  margin-right: 20px;
+}
 </style>
